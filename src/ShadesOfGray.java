0f571d15fc551b1ca2f1fdb91284f21162e45a9e
Joe Rowley <jrowley@vertigo.com>
2015-09-29 17:10:28 -0600

    Add one more puzzler. Time to do more reading!

---
+++ src/ShadesOfGray.java
@@ -0,0 +1,23 @@
+/**
+ * Created by jrowley on 9/29/15.
+ */
+
+public class ShadesOfGray {
+    public static void main(String[] args) {
+//        System.out.println(X.Y.Z);
+    }
+}
+
+/*
+
+class X {
+    static class Y {
+        static String Z = "Black";
+    }
+    static C Y = new C();
+}
+class C {
+    String Z = "White";
+}
+
+*/
